9da2ce9261190bd99273110622a859ba5dc1992b
Will Pirtle <wepirtle1@gmail.com>
2021-04-15 17:49:12 -0700

    Game is functional

---
+++ main.js
@@ -0,0 +1,50 @@
+const options = ['rock', 'paper', 'scissors'];
+let playerScore = 0;
+let computerScore = 0;
+
+function computerPlay() {
+  return options[Math.floor(Math.random() * options.length)];
+}
+
+function getPlayerSelection() {
+  return prompt("Make selection: ").toLowerCase();
+}
+
+function playRound(playerSelection, computerSelection) {
+  if (playerSelection === computerSelection) {
+    return `Draw! Try again.`;
+  } else if (
+      playerSelection === 'rock' && computerSelection === 'scissors'
+      || playerSelection === 'paper' && computerSelection === 'rock'
+      || playerSelection === 'scissors' && computerSelection === 'paper'
+  ) {
+    playerScore++;
+    return `You won! ${playerSelection.toUpperCase()} beats ${computerSelection.toUpperCase()}.`;
+  } else {
+    computerScore++;
+    return `You lose! ${computerSelection.toUpperCase()} beats ${playerSelection.toUpperCase()}.`;
+  }
+}
+
+function resetScores() {
+  playerScore = 0;
+  computerScore = 0;
+}
+
+function startGame() {
+  while (playerScore != 3 && computerScore != 3) {
+    let playerSelection = getPlayerSelection();
+    let computerSelection = computerPlay();
+
+    console.log(playRound(playerSelection, computerSelection));
+
+    console.log(`Player: ${playerScore}\nComputer: ${computerScore}`);
+  }
+  if (playerScore == 3) {
+    console.log("\nCONGRATS! You won the game!")
+    resetScores();
+  }  else {
+    console.log("\nAgghh...Computer wins.")
+    resetScores();
+  }
+}
